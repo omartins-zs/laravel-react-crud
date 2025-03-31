b6c3f8d366ca6917d79052e4dbca0e083dfd7bc7
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-31 11:06:12 -0300

    Criando Metodo Index para Listar Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        return Inertia::render('Posts',[
+            'posts' => Post::all(),
+        ]);
     }
 
     /**
