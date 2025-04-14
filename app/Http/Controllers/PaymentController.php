f01614eda3c60473abdac8465e8b6f42c8f984af
omartins-zs <gabrielmatheus209@gmail.com>
2025-04-14 10:44:08 -0300

    Ajustando Metodos Index,Store,Update e Destroy

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,20 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Payment;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Payment;
 use Inertia\Response;
-
 
 class PaymentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        return Inertia::render('Payments', ['payments' => Payment::all(),]);
+        return Inertia::render('Payments', [
+            'payments' => Payment::orderBy('created_at', 'desc')->get(),
+        ]);
     }
 
     /**
@@ -31,7 +32,21 @@ class PaymentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate the request data
+        $request->validate([
+            'email' => 'required|email|unique:payments,email,',
+            'amount' => 'required|numeric|min:0',
+            'status' => 'required|in:pending,success,failed,processing',
+        ]);
+
+        // Update the payment details
+        Payment::create([
+            'email' => $request->email,
+            'amount' => $request->amount,
+            'status' => $request->status,
+        ]);
+
+        return response()->json(['message' => 'Payment added successfully!'], 200);
     }
 
     /**
@@ -53,16 +68,35 @@ class PaymentController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        // Validate the request data
+        $request->validate([
+            'email' => 'required|email|unique:payments,email,' . $id,
+            'amount' => 'required|numeric|min:0',
+            'status' => 'required|in:pending,success,failed,processing',
+        ]);
+
+        // Find the payment by ID
+        $payment = Payment::findOrFail($id);
+
+        // Update the payment details
+        $payment->update([
+            'email' => $request->email,
+            'amount' => $request->amount,
+            'status' => $request->status,
+        ]);
+
+        return response()->json(['message' => 'Payment updated successfully!'], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $payment = Payment::findOrFail($id);
+        $payment->delete();
+        return  redirect()->back()->with('success', 'Payment Deleted Successfullu');
     }
 }
